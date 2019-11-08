ed0b5f8fb41888c0abefa1e4bd843c02d474b406
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2019-11-08 01:14:47 -0200

    A020 - Criando Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { AppComponent } from "./app.component";
+import { ExemplosPipesComponent } from "./exemplos-pipes/exemplos-pipes.component";
+import { CamelCasePipe } from "./camel-case.pipe";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExemplosPipesComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ExemplosPipesComponent, CamelCasePipe],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
