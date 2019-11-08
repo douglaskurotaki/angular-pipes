28a468cafe986172ffd7d580a369472d6f78f9c8
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2019-11-08 02:07:31 -0200

    A021 - Aplicando localte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
+import { SettingsService } from "./settings.service";
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, LOCALE_ID } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { ExemplosPipesComponent } from "./exemplos-pipes/exemplos-pipes.component";
 import { CamelCasePipe } from "./camel-case.pipe";
+import localePt from "@angular/common/locales/pt";
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData(localePt, "pt-BR");
 
 @NgModule({
   declarations: [AppComponent, ExemplosPipesComponent, CamelCasePipe],
   imports: [BrowserModule],
-  providers: [],
+  providers: [
+    // {
+    //   provide: LOCALE_ID,
+    //   useValue: "pt-BR"
+    // }
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: settingsService => settingsService.getLocale()
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
